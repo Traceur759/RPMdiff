8c31cbca341c01e2e296dff0cef453cae68656c3
Marcel Plch <marcel.plch@seznam.cz>
2017-04-04 08:28:11 +0200

    Downloading of RPMs added

--- diff.py
+++ diff.py
@@ -1,5 +1,5 @@
 from flask import *
-import json
+import sqlite3
 import differ_loader
 
 app = Flask(__name__)
@@ -7,15 +7,26 @@ app.config.from_object(__name__)
 app.config.from_envvar('DIFF_SETTINGS', silent=True)
 
 @app.route('/')
-def show_entries():
+def index():
+    return redirect(url_for('get_rpm_request'))
+
+@app.route('/rpm')
+def get_rpm_request():
     return render_template('rpm_request.html')
 
 @app.route('/diff', methods=['POST'])
 def request_processor():
     pkg1 = request.form['pkg1']
     pkg2 = request.form['pkg2']
+    additional = (request.form['pkg1_release'],
+                  request.form['pkg1_arch'],
+                  request.form['pkg2_release'],
+                  request.form['pkg2_arch']
+                )
     differ = differ_loader.load_differ(pkg1,
-                                       pkg2
+                                       pkg2,
+                                       "RPM",
+                                       additional
                                       )
     text_diff = differ.get_diff()
     return render_template('result.html', result=text_diff)
--- differ_loader.py
+++ differ_loader.py
@@ -1,6 +1,7 @@
 import rpmdiff
 import dnf
 import os
+from shutil import copyfile
 
 class Differ:
     """Abstract class for Differ inheritance"""
@@ -12,28 +13,70 @@ class Differ:
         raise "Method get_diff was not implemented"
 
 class Rpm_Differ(Differ):
-    def __init__(self, pkg1, pkg2):
-        self.pkg1 = pkg1
-        self.pkg2 = pkg2
+    def __init__(self, pkg1, pkg2, additional):
+        self._pkg1 = (pkg1, additional[0], additional[1])
+        self._pkg2 = (pkg2, additional[2], additional[3])
+        self.base = None
         self.diff = None
 
-    def _download_pkg():
-        base = dnf.Base()
+    def _download_pkg(self, pkg):
+        reponame = "{release}-{arch}".format(
+                                            release=pkg[1],
+                                            arch=pkg[2]
+                                        )
+        print("Release:%s\nArch:%s" % (pkg[1], pkg[2]))
+        baseurl = "https://mirrors.nic.cz/fedora/linux/releases/{release}/Everything/{arch}/os/".format(
+                                                                                                     release=pkg[1],
+                                                                                                     arch=pkg[2]
+                                                                                                )
+        self.base = dnf.Base()
+        conf = self.base.conf
+        conf.cachedir = "cachedir"
+        self.base.repos.add_new_repo(reponame,
+                                conf,
+                                baseurl=(baseurl,)
+                               )
+        self.base.fill_sack()
+        q = self.base.sack.query()
+        q = q.available()
+        q = q.filter(name=pkg[0])
+        for package in q:
+            print(package)
+        self.base.download_packages(q)
+
+    def _get_package_path(self, pkg):
+        repodir = "{release}-{arch}".format(
+                                      release=pkg[1],
+                                      arch=pkg[2]
+                                    )
+        for entity in os.listdir("cachedir"):
+            if entity.startswith(repodir) and not (entity.endswith(".solvx") or entity.endswith(".solv")):
+                print("dir: ", entity)
+                repodir = entity
+
+        repodir = "cachedir/{repodir}/packages/".format(repodir=repodir)
+
+        for entity in os.listdir(repodir):
+            print("pkg: ", entity)
+            if entity.startswith(pkg[0]):
+                pkg_name = entity
+        return os.path.join(repodir, pkg_name)
 
     def get_diff(self):
-        text_diff = None
-        rdir = os.getcwd()
-        try:
-            os.chdir("%s/rpms" % rdir)
-            self.diff = rpmdiff.Rpmdiff(self.pkg1,
-                                self.pkg2
-                               )
-            text_diff = self.diff.textdiff()
-        finally:
-            os.chdir(rdir)
-        for r in self.diff.result:
-            print(r)
-        return text_diff
+        to_download = (self._pkg1, self._pkg2)
+        for i, pkg in enumerate(to_download):
+            self._download_pkg(pkg)
+            path = self._get_package_path(pkg)
+            copyfile(path, "rpms/pkg"+ str(i+1) +".rpm")
+            self.base.close()
 
-def load_differ(pkg1 = None, pkg2 = None, category = "RPM"):
-    return Rpm_Differ(pkg1, pkg2)
+
+        self.diff = rpmdiff.Rpmdiff("rpms/pkg1.rpm",
+                                    "rpms/pkg2.rpm"
+                                    )
+        return self.diff.textdiff()
+
+def load_differ(pkg1, pkg2, category, additional = None):
+    if category == "RPM":
+        return Rpm_Differ(pkg1, pkg2, additional)
+    return None
